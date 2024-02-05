1cc1e671d3ab9923dee865ca164a79657e6b30f3
Joyesh Shrestha <sjoyesh2000@gmail.com>
2024-02-05 23:42:46 +0545

    removing bootstrap flask

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, jsonify
-from flask_bootstrap import Bootstrap5
+# from flask_bootstrap import Bootstrap5
 app= Flask(__name__)
 
 
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# Bootstrap5(app)
 
 @app.route('/')
 def hello_world():
